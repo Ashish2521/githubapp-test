fc3c8dbaab25871224c7f6e68d8cbae2a8ad7c0b
Ashish2521 <ashushrm864@gmail.com>
2023-12-24 16:39:44 +0530

    basic functionality added to test

--- scripts/details.py
+++ scripts/details.py
@@ -3,12 +3,21 @@ import requests
 import jwt
 import time
 from jwt import JWT
-import time
-import os
 
+def generate_jwt(private_key, app_id):
+    payload = {
+        'iat': int(time.time()),  # Issued at time
+        'exp': int(time.time()) + 600,  # JWT expiration time (10 minutes maximum)
+        'iss': app_id  # GitHub App's identifier
+    }
 
-def print_repo_details():
+    # Create JWT
+    jwt_instance = JWT()
+    encoded_jwt = jwt_instance.encode(payload, private_key, alg='RS256')
 
+    return encoded_jwt
+
+def print_repo_details(private_key, app_id):
     # Authenticate as the GitHub App
     headers = {
         "Authorization": f"Bearer {generate_jwt(private_key, app_id)}",
@@ -27,26 +36,13 @@ def print_repo_details():
     else:
         print(f"Failed to retrieve repository information. Status code: {repo_response.status_code}")
 
-
-def generate_jwt(private_key, app_id):
-    payload = {
-        'iat': int(time.time()),  # Issued at time
-        'exp': int(time.time()) + 600,  # JWT expiration time (10 minutes maximum)
-        'iss': app_id  # GitHub App's identifier
-    }
-
-    # Create JWT
-    jwt_instance = JWT()
-    encoded_jwt = jwt_instance.encode(payload, private_key, alg='RS256')
-
-    return encoded_jwt
-
 if __name__ == "__main__":
     # Get private key and app ID from GitHub secrets
-    private_key = os.environ.get("APP_PRIVATE_KEY").replace('\\n', '\n')
+    private_key = os.environ.get("PRIVATE_KEY").replace('\\n', '\n')
     app_id = int(os.environ.get("APP_ID"))
 
     jwt_token = generate_jwt(private_key, app_id)
     print(f"JWT:  {jwt_token}")
-    print_repo_details()
-
+    
+    # Print repository details
+    print_repo_details(private_key, app_id)
