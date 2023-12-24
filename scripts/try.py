d118898220b22adfde5405e105408ead55f4b3e7
Ashish2521 <ashushrm864@gmail.com>
2023-12-24 19:36:49 +0530

    basic functionality added to test

--- scripts/try.py
+++ scripts/try.py
@@ -1,48 +1,26 @@
 import os
-import requests
-import jwt
-import time
-
-def generate_jwt(app_id, private_key):
-    now = int(time.time())
-    payload = {
-        # Issued at time
-        'iat': int(time.time()),
-        # JWT expiration time (10 minutes maximum)
-        'exp': int(time.time()) + 600,
-        # GitHub App's identifier
-        'iss': app_id
-    }
-
-    encoded_jwt = jwt.encode(payload, private_key, algorithm='RS256')
-
-    return encoded_jwt
+from github import Github
+from github.GithubException import UnknownObjectException
 
 def print_repo_details(app_id, private_key):
-    # Authenticate as the GitHub App
-    jwt_token = generate_jwt(app_id, private_key)
-    headers = {
-        "Authorization": f"Bearer {jwt_token}",
-        "Accept": "application/vnd.github.v3+json",
-    }
+    print(f"APP_ID: {app_id}")
 
-    # Use the GitHub REST API to get repository information
-    repo_url = f"https://api.github.com/repos/Ashish2521/githubapp-test"
-    repo_response = requests.get(repo_url, headers=headers)
-    print(repo_response.text)
-    
-    if repo_response.status_code == 200:
-        repo_data = repo_response.json()
-        # Print repository name and language
-        print(f"Repository Name: {repo_data['name']}")
-        print(f"Repository Language: {repo_data['language']}")
-        print(f"Response body: {repo_response.text}")
-    else:
-        print(f"Failed to retrieve repository information. Status code: {repo_response.status_code}")
+    try:
+        # Authenticate as the GitHub App using PyGithub
+        g = Github(base_url="https://api.github.com", login_or_token=app_id, private_key=private_key)
+
+        # Get repository information
+        repo = g.get_repo("Ashish2521/githubapp-test")
+
+        # Print repository details
+        print(f"Repository Name: {repo.name}")
+        print(f"Repository Language: {repo.language}")
+    except UnknownObjectException as e:
+        print(f"Failed to retrieve repository information. Error: {str(e)}")
 
 if __name__ == "__main__":
     # Get app ID and private key from GitHub secrets
-    app_id = int(os.environ.get("APP_ID"))
+    app_id = os.environ.get("APP_ID")
     private_key = os.environ.get("APP_PRIVATE_KEY")
 
     # Print repository details
