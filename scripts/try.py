c4dabcbd3f582b0d5788caca709ca5d3b7e59171
Ashish2521 <ashushrm864@gmail.com>
2023-12-25 15:11:37 +0530

    basic functionality added to test

--- scripts/try.py
+++ scripts/try.py
@@ -48,7 +48,7 @@ def get_access_token_details():
         
         if installation_response.status_code == 200:
             installation_data = installation_response.json()
-            print(installation_data[0]['id'])
+            print(installation_data[0])
             installation_id = installation_data[0]['id']
             access_token_url = f"https://api.github.com/app/installations/{installation_id}/access_tokens"
 
