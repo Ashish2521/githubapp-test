c165b174e31d83bde483f7db97f5b0e8e1baf400
Ashish2521 <ashushrm864@gmail.com>
2023-12-26 22:12:31 +0530

    basic functionality added to test

--- scripts/try.py
+++ scripts/try.py
@@ -81,7 +81,7 @@ def get_repo_details(access_token, owner, repo):
         "Authorization": f"Bearer {access_token}",
         "Accept": "application/vnd.github.v3+json",
     }
-    repo_url = f"https://api.github.com/repos/{owner}/tagcompliance"
+    repo_url = f"https://api.github.com/users/Ashish2521"
 
     try:
         repo_response = requests.get(repo_url, headers=headers)
