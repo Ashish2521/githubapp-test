b7c9044fc35a8fefb87cf68c67512dcad4d84ae3
Ashish2521 <ashushrm864@gmail.com>
2023-12-26 21:24:49 +0530

    basic functionality added to test

--- scripts/try.py
+++ scripts/try.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python3
 from jwt import JWT, jwk_from_pem
 import time
-import sys
-import os,requests
+import os
+import requests
 
 def generate_jwt():
     # Get PEM file path
     pem = os.path.join(os.path.dirname(__file__), "private-key.pem")
 
     # Get the App ID
-    app_id = int(os.environ.get("APP_ID")) 
+    app_id = int(os.environ.get("APP_ID"))
 
     # Open PEM
     with open(pem, 'rb') as pem_file:
@@ -29,67 +28,95 @@ def generate_jwt():
     encoded_jwt = jwt_instance.encode(payload, signing_key, alg='RS256')
     return encoded_jwt
 
-def get_access_token_details():
-    # Authenticate as the GitHub App
-    jwt_token = generate_jwt()
-    if jwt_token is None:
-        print("Authentication failed. Check previous error messages for details.")
-        return
+def get_installation_id(jwt_token):
+    # Get installation ID
     headers = {
         "Authorization": f"Bearer {jwt_token}",
         "Accept": "application/vnd.github.v3+json",
         "X-GitHub-Api-Version": "2022-11-28",
     }
-    access_token_url = f"https://api.github.com/app/installations/45427177/access_tokens"
+    app_installations_url = f"https://api.github.com/app/installations"
+
+    try:
+        installations_response = requests.get(app_installations_url, headers=headers)
+        installations_response.raise_for_status()
+
+        installations_data = installations_response.json()
+        print(installations_data)
+        if installations_data and len(installations_data) > 0:
+            return installations_data[0]['id']  # Assuming you want the first installation ID
+        else:
+            print("No installations found.")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
+def get_access_token_details(installation_id, jwt_token):
+    # Authenticate as the GitHub App using installation ID
+    headers = {
+        "Authorization": f"Bearer {jwt_token}",
+        "Accept": "application/vnd.github.v3+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    access_token_url = f"https://api.github.com/app/installations/{installation_id}/access_tokens"
 
     try:
         access_token_response = requests.post(access_token_url, headers=headers)
         access_token_response.raise_for_status()
-        print(access_token_response.status_code)
 
         if access_token_response.status_code == 201:
             access_token_data = access_token_response.json()
-            print(access_token_data)
+            return access_token_data.get('token')
+        else:
+            print(f"Failed to obtain access token. Status code: {access_token_response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+def get_repo_details(access_token, owner, repo):
+    # Get repository details
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Accept": "application/vnd.github.v3+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    repo_url = f"https://api.github.com/repos/{owner}/{repo}"
+
+    try:
+        repo_response = requests.get(repo_url, headers=headers)
+        repo_response.raise_for_status()
+
+        if repo_response.status_code == 200:
+            repo_data = repo_response.json()
+            print(f"Repository Details:")
+            print(f"Name: {repo_data['name']}")
+            print(f"Description: {repo_data['description']}")
+            print(f"URL: {repo_data['html_url']}")
+            print(f"Created At: {repo_data['created_at']}")
+        else:
+            print(f"Failed to retrieve repository details. Status code: {repo_response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
 
-    # # Use the GitHub REST API to get repository information
-    # installation_url = f"https://api.github.com/app/installations"
-    # try:
-    #     app_id = int(os.environ.get("APP_ID")) 
-    #     installation_response = requests.get(installation_url, headers=headers)
-    #     installation_response.raise_for_status()
-        
-    #     if installation_response.status_code == 200:
-    #         installation_data = installation_response.json()
-    #         installation_id = installation_data[0]['id']
-    #         print(installation_id)
-    #         access_token_url = f"https://api.github.com/app/installations/{installation_id}/access_tokens"
-    #         jwt_token = generate_jwt()
-    #         if jwt_token is None:
-    #             print("Authentication failed. Check previous error messages for details.")
-    #             return
-    #         headers = {
-    #             "Authorization": f"Bearer {jwt_token}",
-    #             "Accept": "application/vnd.github.v3+json",
-    #         }
-
-    #         try:
-    #             access_token_response = requests.get(access_token_url, headers=headers)
-    #             access_token_response.raise_for_status()
-
-    #             if access_token_response.status_code == 200:
-    #                 access_token_data = access_token_response.json()
-    #                 print(access_token_data)
-    #         except requests.exceptions.RequestException as e:
-    #             print(f"Request failed: {e}")
-    #     else:
-    #         print(f"Failed to retrieve repository information. Status code: {installation_response.status_code}")
-
-    # except requests.exceptions.RequestException as e:
-    #     print(f"Request failed: {e}")
-
 if __name__ == "__main__":
-    
-    # Print repository details
-    access_token = get_access_token_details()
+    # Generate JWT token
+    jwt_token = generate_jwt()
+
+    # Get installation ID
+    installation_id = get_installation_id(jwt_token)
+    if installation_id is not None:
+        print(f"Installation ID: {installation_id}")
+
+        # Get access token using installation ID
+        access_token = get_access_token_details(installation_id, jwt_token)
+        if access_token is not None:
+            print(f"Access Token: {access_token}")
+
+            owner = "Ashish2521" 
+            repo = "githuapp-test" 
+            get_repo_details(access_token, owner, repo)
+        else:
+            print("Failed to obtain access token.")
+    else:
+        print("Failed to obtain installation ID.")
